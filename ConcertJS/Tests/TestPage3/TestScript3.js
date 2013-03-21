52363c7af6a33b41a13284e563c5b409db1e1c1c
Trajan McGill <code@trajanmcgill.com>
2013-03-21 10:32:29 -0500

    Updated some test functions to match interface changes. Issue: #10

--- ConcertJS/Tests/TestPage3/TestScript3.js
+++ ConcertJS/Tests/TestPage3/TestScript3.js
@@ -8,59 +8,51 @@ var sequence, sequence2, syncSource;
 
 function init()
 {
-	var i, j, x1, y1, x2, y2, $testArea, $curdiv, curTransformationList, transformationSet = [];
-	var timeSoFar;
-	var numSegments, thisSegmentTime;
+	var i, j, x, y, $testArea, $curdiv, curTimesArry, curValuesArray, transformationSet = [];
+	var lastKeyframeTime;
+	var numSegments, thisKeyframeTime;
 
 	var numDots = 100;
 	var numSegmentsMin = 100, numSegmentsMax = 100;
 	var segmentTimeMin = 100, segmentTimeMax = 5000;
-	//var totalTime = 10000;
 	var easingFunction = Concert.EasingFunctions.Smoothstep;
-	//var segmentTime = totalTime / numSegments;
 
 	$testArea = $("#TestArea");
 	for (i = 0; i < numDots; i++)
 	{
 		$curdiv = $("<div>").addClass("dot").appendTo($testArea);
 
-		x1 = Math.floor(Math.random() * 800);
-		y1 = Math.floor(Math.random() * 800);
 		numSegments = numSegmentsMin + Math.floor(Math.random() * (numSegmentsMax - numSegmentsMin))
 
-		curTransformationList = [];
-		timeSoFar = 0;
-		for(j = 0; j < numSegments; j++)
+		lastKeyframeTime = 0;
+		curTimesArry = [lastKeyframeTime];
+		x = Math.floor(Math.random() * 800);
+		y = Math.floor(Math.random() * 800);
+		curValuesArray = [[x, y]];
+		for (j = 0; j < numSegments; j++)
 		{
-			x2 = Math.floor(Math.random() * 800);
-			y2 = Math.floor(Math.random() * 800);
-			thisSegmentTime = segmentTimeMin + Math.floor(Math.random() * (segmentTimeMax - segmentTimeMin));
+			lastKeyframeTime = lastKeyframeTime + segmentTimeMin + Math.floor(Math.random() * (segmentTimeMax - segmentTimeMin));
 
-			curTransformationList.push(
-				{
-					startTime: timeSoFar,
-					endTime: timeSoFar + thisSegmentTime,
-					easingFunction: easingFunction,
-					calculator: Concert.Calculators.Linear,
-					startValue: [x1, y1],
-					endValue: [x2, y2],
-					unit: ["px", "px"]
-				});
+			x = Math.floor(Math.random() * 800);
+			y = Math.floor(Math.random() * 800);
 
-			x1 = x2;
-			y1 = y2;
-			timeSoFar += thisSegmentTime;
+			curTimesArry.push(lastKeyframeTime);
+			curValuesArray.push([x, y]);
 		}
 
 		transformationSet.push(
 			{
-				objectTarget: $curdiv.get(0),
-				objectSequences:
-					[{
-						featureTarget: ["left", "top"],
-						applicator: Concert.Applicators.Style,
-						transformations: curTransformationList
-					}]
+				target: $curdiv.get(0),
+				feature: ["left", "top"],
+				easing: easingFunction,
+				calculator: Concert.Calculators.Linear,
+				applicator: Concert.Applicators.Style,
+				unit: "px",
+				keyframes:
+					{
+						times: curTimesArry,
+						values: curValuesArray
+					}
 			});
 	}
 
@@ -69,122 +61,9 @@ function init()
 	sequence.seek(0);
 }
 
+
 function init2()
-{
-	var $d1 = $("<div>1</div>").css({ position: "absolute", left: "100px", top: "100px", width: "50px", height: "50px", backgroundColor:"#ff0000" });
-	var $d2 = $("<div>2</div>").css({ position: "absolute", left: "200px", top: "100px", width: "50px", height: "50px", backgroundColor: "rgb(0, 255, 0)" });
-	var $d3 = $("<div>3</div>").css({ position: "absolute", left: "300px", top: "100px", width: "50px", height: "50px", backgroundColor: "hsl(90, 100%, 50%)" });
-	var $d4 = $("<div>4</div>").css({ position: "absolute", left: "400px", top: "100px", width: "50px", height: "50px", backgroundColor: "rgba(0, 0, 255, 1)" });
-	var $d5 = $("<div>5</div>").css({ position: "absolute", left: "500px", top: "100px", width: "50px", height: "50px", backgroundColor: "hsla(270, 100%, 50%, 1)" });
-
-	var $testArea = $("#TestArea");
-
-	$testArea.append($d1).append($d2).append($d3).append($d4).append($d5);
-
-	sequence = new Concert.Sequence(
-		[
-			{
-				objectTarget: $d1.get(0),
-				objectSequences:
-					[{
-						featureTarget: "background-color",
-						applicator: Concert.Applicators.Style,
-						transformations:
-							[
-								{
-									startTime: 0,
-									endTime: 4000,
-									easingFunction: Concert.EasingFunctions.ConstantRate,
-									calculator: Concert.Calculators.Color,
-									startValue: "#000000",
-									endValue: "#f00"
-								}
-							]
-					}]
-			},
-
-			{
-				objectTarget: $d2.get(0),
-				objectSequences:
-					[{
-						featureTarget: "background-color",
-						applicator: Concert.Applicators.Style,
-						transformations:
-							[
-								{
-									startTime: 0,
-									endTime: 4000,
-									easingFunction: Concert.EasingFunctions.ConstantRate,
-									calculator: Concert.Calculators.Color,
-									startValue: "rgb(0, 0, 0)",
-									endValue: "rgb(0, 255, 0)"
-								}
-							]
-					}]
-			},
-
-			{
-				objectTarget: $d3.get(0),
-				objectSequences:
-					[{
-						featureTarget: "background-color",
-						applicator: Concert.Applicators.Style,
-						transformations:
-							[
-								{
-									startTime: 0,
-									endTime: 4000,
-									easingFunction: Concert.EasingFunctions.ConstantRate,
-									calculator: Concert.Calculators.Color,
-									startValue: "hsl(0, 0%, 50%)",
-									endValue: "hsl(90, 100%, 100%)"
-								}
-							]
-					}]
-			},
-
-			{
-				objectTarget: $d4.get(0),
-				objectSequences:
-					[{
-						featureTarget: "background-color",
-						applicator: Concert.Applicators.Style,
-						transformations:
-							[
-								{
-									startTime: 0,
-									endTime: 4000,
-									easingFunction: Concert.EasingFunctions.ConstantRate,
-									calculator: Concert.Calculators.Color,
-									startValue: "rgba(0, 255, 0, 0)",
-									endValue: "rgba(0, 0, 255, 1)"
-								}
-							]
-					}]
-			},
-
-			{
-				objectTarget: $d5.get(0),
-				objectSequences:
-					[{
-						featureTarget: "background-color",
-						applicator: Concert.Applicators.Style,
-						transformations:
-							[
-								{
-									startTime: 0,
-									endTime: 4000,
-									easingFunction: Concert.EasingFunctions.ConstantRate,
-									calculator: Concert.Calculators.Color,
-									startValue: "hsla(0, 100%, 50%, 1)",
-									endValue: "hsla(360, 100%, 50%, 1)"
-								}
-							]
-					}]
-			}
-		]);
-	sequence.index();
-}
+{}
 
 
 function init3()
@@ -208,48 +87,11 @@ function init3()
 					{
 						times: [0, 1000, null, 2000, 3000],
 						values: [[100, 50], [200, 50], null, [200, 50], [200, 100]]
-						//,
-						//valueGenerators: [function (sequence) { return [300, 300]; }, null, null, null, null]
 					}
 			}
 		]);
 	
-	/*
-	sequence.addTransformations(
-		[
-			{
-				target: $d1.get(0),
-				feature: ["top", "width"],
-				unit: "px",
-				segments:
-					[
-						{ t1: 0, t2: 1500, v1: [0, 100], v2: [100, 1000], v1Generator: function (sequence) { return [300, 10] } },
-						{ t1: 2000, t2: 3500, v1: [100, 100], v2: [100, 1000] }
-					]
-			}
-		]);
-	*/
 	sequence.index();
-
-	//sequence2 = sequence.clone(function (oldTarget) { return $d2.get(0); });
-
-	/*
-	sequence.retarget(
-		function (oldTarget)
-		{
-			if (oldTarget == "d1")
-				return $d1.get(0);
-			else if (oldTarget == "d2")
-				return $d2.get(0);
-			else if (oldTarget == "d3")
-				return $d3.get(0);
-			else if (oldTarget == "caption")
-				return $caption;
-			else
-				return oldTarget;
-		});
-
-	*/
 }
 
 
@@ -315,7 +157,6 @@ function beginClick()
 
 	sequence.begin(
 		{
-			generateValues: true,
 			onAutoStop: function () { jQuery("#StatusLabel").text("Auto-stopped."); },
 			useSoleControlOptimization: true
 		});
