5aa22beb014a7059b1de830469f98e663091af1a
Trajan McGill <code@trajanmcgill.com>
2013-03-21 21:23:54 -0500

    Reworked RequestAnimationFrame to make it better, get it to pass jshint, fix some trouble with using it in IE 7, and generally optimize it. Issue: #26, #15

--- ConcertJS/Components/RequestAnimationFrame/RequestAnimationFrame.js
+++ ConcertJS/Components/RequestAnimationFrame/RequestAnimationFrame.js
@@ -2,64 +2,73 @@
 {
 	"use strict";
 
-	var callbackList = [];
-	var unusedIDs = [];
-	var timeoutID = null;
-	var lastFrameTime = 0;
+	var nextID = 0, callbackAddQueue = {}, callbackProcessingQueue = {}, timeoutID = null, lastFrameTime = 0, callbackCount = 0, firstNewID = 0;
 
+	// request(): This function is exported as window.requestAnimationFrame if it isn't already defined.
+	// Adds a new animation frame callback to the queue of requests waiting for the next frame.
+	// Returns an integer that uniquely identifies this request.
 	function request(callback)
 	{
-		var newID = (unusedIDs.length > 0) ? unusedIDs.pop() : callbackList.length;
-		callbackList[newID] = callback;
+		var newID = nextID;
+		nextID++;
+
+		callbackAddQueue[newID] = callback;
+		callbackCount++;
+
 		if (timeoutID === null)
-			timeoutID = window.setTimeout(doProcessing, Math.max(0, 16 - ((new Date()).getTime() - lastFrameTime)));
+			timeoutID = window.setTimeout(_doProcessing, Math.max(0, 16 - ((new Date()).getTime() - lastFrameTime)));
+
 		return newID;
 	}
 
-
+	// cancel(): This function is exported as window.cancelAnimationFrame if it isn't already defined.
+	// Cancels the outstanding frame request identified by the specified ID.
 	function cancel(requestID)
 	{
-		if (callbackList[requestID])
-			cancelUnchecked(requestID);
+		if (requestID >= firstNewID && callbackAddQueue.hasOwnProperty(requestID))
+			_cancelFrom(callbackAddQueue, requestID);
+		else if (callbackProcessingQueue.hasOwnProperty(requestID))
+			_cancelFrom(callbackProcessingQueue, requestID);
 	}
 
-
-	function cancelUnchecked(requestID)
+	// _cancelFrom(): Internal function that removes an outstanding frame request from a particular queue.
+	function _cancelFrom(queue, requestID)
 	{
-		callbackList[requestID] = null;
-		if (unusedIDs.length === callbackList.length - 1)
+		delete queue[requestID];
+		callbackCount--;
+		if (callbackCount < 1 && timeoutID !== null)
 		{
-			if (timeoutID !== null)
-			{
-				window.clearTimeout(timeoutID);
-				timeoutID = null;
-			}
-			callbackList = [];
-			unusedIDs = [];
+			window.clearTimeout(timeoutID);
+			timeoutID = null;
 		}
-		else
-			unusedIDs.push(requestID);
 	}
 
-
-	function doProcessing()
+	// _doProcessing(): Internal function that processes the queue and makes all the callbacks.
+	function _doProcessing()
 	{
-		var i, currentCallback;
-		lastFrameTime = (new Date()).getTime();
-		for (i = 0; i < callbackList.length; i++)
-		{
-			currentCallback = callbackList[i];
-			if (currentCallback !== null)
-			{
-				cancelUnchecked(i);
+		var requestID, currentCallback;
 
+		timeoutID = null;
+		lastFrameTime = (new Date()).getTime();
+
+		callbackProcessingQueue = callbackAddQueue;
+		callbackAddQueue = {};
+		firstNewID = nextID;
+
+		for (requestID in callbackProcessingQueue)
+		{
+			if (callbackProcessingQueue.hasOwnProperty(requestID))
+			{
+				currentCallback = callbackProcessingQueue[requestID];
+				delete callbackProcessingQueue[requestID];
+				callbackCount--;
 				try { currentCallback(lastFrameTime); }  // Exactly what value is passed to the callback function may still be changing in the spec.
 				catch (e) { }
 			}
 		}
 	}
 
-
+	// Export the requestAnimationFrame() and cancelAnimationFrame() functions if appropriate.
 	if (!(window.requestAnimationFrame))
 	{
 		// Note: The spec actually calls for there to be just one task, at the top-level browsing context,
@@ -68,4 +77,5 @@
 		window.requestAnimationFrame = request;
 		window.cancelAnimationFrame = cancel;
 	}
+
 })();
