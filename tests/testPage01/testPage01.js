8481cec5256ab95c30085fead7897283d8f4aaea
Trajan McGill <code@trajanmcgill.com>
2019-11-09 16:20:30 -0600

    Added a very basic web page for testing things.
    Issue: #70

---
+++ tests/testPage01/testPage01.js
@@ -0,0 +1,32 @@
+﻿(function ()
+{
+	"use strict";
+
+	var sequence = new Concert.Sequence();
+
+	sequence.addTransformations(
+		{
+			target: document.getElementById("MovingDiv"),
+			feature: ["left", "top"],
+			unit: ["px", "px"],
+			applicator: Concert.Applicators.Style,
+			calculator: Concert.Calculators.Rotational, /*
+			keyframes:
+			{
+				times: [0, 2000],
+				values:
+				[
+					{ center: { left: 400, top: 300 }, offset: { left: -25, top: -25 }, radius: 25, angle: 0 },
+					{ center: { left: 400, top: 300 }, offset: { left: -25, top: -25 }, radius: 25, angle: 2 * Math.PI }
+				]
+			} */
+			keyframes: { times: [0, 2000], values: [[25, 0], [25, 2*Math.PI]] },
+			userProperties:
+			{
+				center: [400, 300],
+				offset: [-25, -25]
+			}
+		});
+
+	document.getElementById("GoButton").onclick = function () { sequence.begin(); };
+})();
