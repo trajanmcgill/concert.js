6c0da21cd5ae014a0d3a3b4354f9bf7bee3d65d1
Trajan McGill <code@trajanmcgill.com>
2020-02-14 20:06:46 -0600

    Updated project version number to 1.0.1.

--- src/Concert.js
+++ src/Concert.js
@@ -1,7 +1,7 @@
 ﻿/**
  * @file Concert.js: Easy synchronized animation with JavaScript.
  * @name Concert.js
- * @version 1.0.0
+ * @version 1.0.1
  * @author Trajan McGill <code@trajanmcgill.com>
  */
 
